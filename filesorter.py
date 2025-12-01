99c5d716a2c5b7374d8ac08b6bc994818ec13f9c
tautaviciuteG <greta.tautaviciute@gmail.com>
2025-12-01 15:09:26 +0200

    File Sorter upload

---
+++ filesorter.py
@@ -0,0 +1,22 @@
+import os, shutil
+
+path = r"C:/Users/greta/Desktop/Data_Youtube/Python/"
+file_name = os.listdir(path)
+
+folder_names = ['csv files', 'image files', 'txt files']
+
+for loop in range(0,3):
+    if not os.path.exists(path + folder_names[loop]):
+        print((path + folder_names[loop]))
+        os.makedirs(path + folder_names[loop])
+
+for file in file_name:
+    if ".csv" in file and not os.path.exists(path + "csv files/" + file):
+        shutil.move(path + file, path + "csv files/" + file)
+    elif ".png" in file and not os.path.exists(path + "image files/" + file):
+        shutil.move(path + file, path + "image files/" + file)
+    elif ".txt" in file and not os.path.exists(path + "txt files/" + file):
+        shutil.move(path + file, path + "txt files/" + file)
+
+
+
